2beff3f81336221dbc94e1845e3559a66bba4edb
Radek Kargul <r.kargul@student.tudelft.nl>
2020-02-25 20:49:05 +0100

    Initial commit

---
+++ src/main/java/com/assignment/lunatech/LunatechApplication.java
@@ -0,0 +1,13 @@
+package com.assignment.lunatech;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LunatechApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LunatechApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/assignment/lunatech/LunatechApplicationTests.java
@@ -0,0 +1,13 @@
+package com.assignment.lunatech;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LunatechApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
